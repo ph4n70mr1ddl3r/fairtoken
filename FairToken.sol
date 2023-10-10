43f49471c7381c276d7c5c714162892c943a382d
ph4n70mr1ddl3r <105593742+ph4n70mr1ddl3r@users.noreply.github.com>
2023-10-10 09:44:57 +0800

    Update FairToken.sol

--- FairToken.sol
+++ FairToken.sol
@@ -6,7 +6,7 @@ import "ERC20Burnable.sol";
 
 contract FairToken is ERC20, ERC20Burnable {
     uint256 public pool = 0;
-    uint16 public reward = 20000;
+    uint16 public increment = 20000;
     uint256 public lastblock = 0;
 
     constructor() ERC20("FAIR", "FAIR") {}
@@ -17,18 +17,18 @@ contract FairToken is ERC20, ERC20Burnable {
         if (block.number >= 110700000) {
 
             if(lastblock + 1800 < block.number) {
-                pool = pool + reward*(10 ** uint256(decimals()));
+                pool = pool + increment*(10 ** uint256(decimals()));
                 lastblock = block.number;
-                if (reward > 0) reward = reward - 1;
+                if (increment > 0) increment = increment - 1;
             }
 
-            uint256 tosend = pool / 1000;
+            uint256 claim = pool / 1000;
 
-            if ((tosend == 0) && (pool > 0)) tosend = 1;
+            if ((claim == 0) && (pool > 0)) claim = 1;
 
-            if (tosend > 0) {
-                _mint(msg.sender, tosend);
-                pool = pool - tosend;
+            if (claim > 0) {
+                _mint(msg.sender, claim);
+                pool = pool - claim;
             }
         }
     }    
